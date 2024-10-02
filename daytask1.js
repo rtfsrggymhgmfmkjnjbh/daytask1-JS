abc6b5362a130c691308f954ea8108f10ba2a323
nagalakshmi  jadda <jaddanagalakshmi77@gmail.com>
2024-10-02 23:13:23 +0530

    Add files via upload

---
+++ daytask1.js
@@ -0,0 +1,24 @@
+// var x=10;
+// var y=5;
+// console.log(x);
+// console.log(y);
+
+// var name1 ="naga";
+// var name2 ="jadda";
+// console.log(name1,name2);
+
+var x=20;
+var y=40;
+console.log(x);
+console.log(y);
+
+var a="naga";
+var b="hari";
+var c="suni";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
